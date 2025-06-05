ed2ab598fdeaa0f5789aadf475a629849aad1318
TheMacPotato <holiday_lambert06@icloud.com>
2025-06-05 10:11:31 +0900

    add: HowTo

---
+++ ScienceCustomKeyboard/VideoPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  VideoPlayerView.swift
+//  ScienceCustomKeyboard
+//
+//  Created by Yoshiki on 2024/07/11.
+//
+
+import SwiftUI
+
+struct VideoPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VideoPlayerView()
+}
